9e57f8361d116f9b4d5008890f98babb7bebe23f
chrissou78 <c.fourquier@free.fr>
2026-05-13 17:31:53 +0200

    add dotenv config to api startup

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+﻿require("dotenv").config();
 require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
@@ -64,3 +65,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`MamaTokens API running on port ${PORT}`);
 });
+
